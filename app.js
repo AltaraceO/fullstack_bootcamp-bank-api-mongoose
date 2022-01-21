feb51f5552c2f52744369ced048323408ec99bf9
AltaraceO <orialtarace@gmail.com>
2022-01-21 19:46:22 +0200

    heroku, hopefully

--- app.js
+++ app.js
@@ -7,15 +7,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(userRouter);
-const PORT = process.env.PORT || 5000;
-
 const publicPath = path.join(__dirname, "client/bulid");
 app.use(express.static(publicPath));
-
 app.get("/api", (req, res) => {
   res.send("working girl!");
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}`);
 });
--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 let localUrl = "http://localhost:5000/";
 
 if (process.env.NODE_ENV === "production") {
-  localUrl = "api";
+  localUrl = "/";
 }
 
 export default axios.create({
