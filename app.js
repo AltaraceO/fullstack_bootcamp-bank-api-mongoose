644410fcb79178be3b4e0a1be58474707caa86c9
AltaraceO <orialtarace@gmail.com>
2022-01-21 16:37:50 +0200

    postman check, compass check, react check

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-
 const app = express();
-
-const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cors());
+const PORT = process.env.PORT || 5000;
 
 const publicPath = path.join(__dirname, "client/bulid");
-app.use(cors());
 app.use(express.static(publicPath));
 
-app.use(express.json());
+app.get("/api", (req, res) => {
+  res.send("working girl!");
+});
 
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is up on port ${PORT}`);
 });
--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import localUrl from "./api/Api";
 
 function App() {
-  return <div className="App">Hey world!</div>;
+  const getReq = async () => {
+    const data = await localUrl.get("/");
+    console.log(data.data);
+  };
+
+  return (
+    <div className="App">
+      Hey world hey girrrl!!!
+      <div>
+        <button onClick={getReq}>click</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
