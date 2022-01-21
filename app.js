f7d53097885280b43a1498bc549d0312ddde33e4
AltaraceO <orialtarace@gmail.com>
2022-01-21 21:19:57 +0200

    heroku

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get("/api", (req, res) => {
   res.send("working girl!");
 });
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
